158962a5c6b9c6ecbd67ed5d060e8f5f79e7c6b7
krishna.kunaldd <krishna.kunal@datadoghq.com>
2026-06-20 01:08:08 +0200

    Adds python dep to the cd workflow again

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, dumb human! :) ',
+        'message': 'You are doing great, dumbest human! :) ',
         'deployed_on': 'kubernetes'
     })
 
