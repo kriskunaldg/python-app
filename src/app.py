c02ef166c597934c054338e8f5eb9299e2757a1f
krishna.kunaldd <krishna.kunal@datadoghq.com>
2026-06-20 00:18:24 +0200

    Changes cd runs on to runner-set

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, dumboo human! ',
+        'message': 'You are doing great, dumboooo human! ',
         'deployed_on': 'kubernetes'
     })
 
