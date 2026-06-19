624ae146c7cb2a7fa2f270bdc85609730f6b8343
krishna.kunaldd <krishna.kunal@datadoghq.com>
2026-06-20 01:20:14 +0200

    Adds python dependency to the cd workflow

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, cute human! :) ',
+        'message': 'You are doing great, hihi human! :) ',
         'deployed_on': 'kubernetes'
     })
 
